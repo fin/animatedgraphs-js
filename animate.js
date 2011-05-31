b0e339b77ab17a4d41f481fcba3827535eea865f
fin <fin+github@xbhd.org>
2011-05-31 18:20:04 +0200

    this is not a change; this is just a retab!

--- animate.js
+++ animate.js
@@ -1,40 +1,40 @@
 function AG(datasets, labels) {
-	var self = this;
+    var self = this;
 
-	self.datasets = datasets;
-	self.labels = labels;
+    self.datasets = datasets;
+    self.labels = labels;
 
-	self.dataset = null;
-	self.current_chart_type = null;
-	self.current_elements = null;
-
-
-	self.chart_functions =  {
-					'bar_grouped': raphael.g.barchart,
-				};
+    self.dataset = null;
+    self.current_chart_type = null;
+    self.current_elements = null;
 
 
-	self.set_dataset = function(label) {
-		self.dataset = self.datasets[labels.indexOf(label)];
-	};
+    self.chart_functions =  {
+                    'bar_grouped': raphael.g.barchart,
+                };
 
-	self.dataset_to_input = function(labels) {
 
-	};
+    self.set_dataset = function(label) {
+        self.dataset = self.datasets[labels.indexOf(label)];
+    };
 
-	self.chart = function(chart_type, labels) {
-		var old_chart_type = chart_type;
-		var old_elements = self.current_elements;
+    self.dataset_to_input = function(labels) {
 
-		var chart_function = self.chart_functions[chart_type];
+    };
 
-		chart_function(10,10,300,300, data);
-		
-		if(old_elements) {
-			// TBI
-		} else {
-		}
-	};
+    self.chart = function(chart_type, labels) {
+        var old_chart_type = chart_type;
+        var old_elements = self.current_elements;
+
+        var chart_function = self.chart_functions[chart_type];
+
+        chart_function(10,10,300,300, data);
+        
+        if(old_elements) {
+            // TBI
+        } else {
+        }
+    };
 }
 
 
